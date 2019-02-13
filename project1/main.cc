56189a455e2a386681cd3b7bdd42d949f4589dd5
sangmin5lee <47581216+sangmin5lee@users.noreply.github.com>
2019-02-13 14:49:00 +0900

    Update main.cc
    Revised by the review comment

--- project1/main.cc
+++ project1/main.cc
@@ -4,35 +4,41 @@
 
 using namespace std;
 
+static const int kCustomerNumberMinimum = -500;
+static const int kCustomerNumberMaximum = 2000;
+
 template <typename T>
-void pushQueue(templatePriorityQueue<T>* Que, int data){
+static void pushQueue(templatePriorityQueue<T>* Que, int data){
 	cout << "[+] Integer "<< data <<" pushed in the Priority Queue." << endl;
 	Que->push(data);
 }
 
 template<typename T>
-void popQueue(templatePriorityQueue<T>* Que){
+static void popQueue(templatePriorityQueue<T>* Que){
 	cout << "[+] Integer "<< Que->top() << " will be poped from the Priority Queue." << endl;
 	Que->pop();
+}
+
+static void multipopQueue(templatePriorityQueue<T>* Que, const int count){
+	for(int i = 0 ; i < count ; ++i){
+		popQueue(Que);
+	}
 }
 
 int main(int argc, char* argv[]){
 	templatePriorityQueue<int> priQueue;
 	int customNumber = 0;
 
-	//empty check
-	if(priQueue.empty()){
-		cout << "[!] Priority Queue is empty - initalized" << endl;
-	}
-
-	// push
+	// push with initial condition(23, 598, 302)
+	// If the more information for those values or the exact condition for initial values is given,
+	// the below three lines will be updated.
 	pushQueue(&priQueue,23);
 	pushQueue(&priQueue,598);
 	pushQueue(&priQueue,302);
 
 	if( argc > 1 ){
 		customNumber = atoi(argv[1]);
-		if( customNumber > -500 && customNumber < 2000 ){
+		if( customNumber > kCustomerNumberMinimum && customNumber < kCustomerNumberMaximum ){
 			pushQueue(&priQueue,customNumber);
 		}
 		else{
@@ -53,8 +59,7 @@ int main(int argc, char* argv[]){
 	}
 
 	// pop twice
-	popQueue(&priQueue);
-	popQueue(&priQueue);
+	multipopQueue(&priQueue, 2);
 
 	cout << "[+] Now, Top element of Priority Queue is " << priQueue.top() << endl;
 	
